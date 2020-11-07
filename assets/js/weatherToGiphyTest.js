3c36d7b0e1c82bc9d42d1968b3c2f96702c16527
smary <sziebe@gmail.com>
2020-11-07 09:54:10 -0600

    Added weatherToGiplyTest.js lookup datastructure and quick test

---
+++ assets/js/weatherToGiphyTest.js
@@ -0,0 +1,24 @@
+let weatherToGiphyMap = {
+    "01d" : "clear sky, blue sky, good weather, nice day",
+    "01n" : "clear sky, blue sky, good weather, nice day",
+    "02d" : "clouds",
+    "02n" : "clouds",
+    "03d" : "clouds",
+    "03n" : "clouds",
+    "04d" : "clouds",
+    "04n" : "clouds",
+    "09d" : "rainy",
+    "09n" : "rainy",
+    "10d" : "rainy",
+    "10n" : "rainy",
+    "11d" : "stormy weather",
+    "11n" : "stormy weather",
+    "13d" : "snowy weather",
+    "13n" : "snowy weather",
+    "50d" : "foggy mist",
+    "50n" : "foggy mist"
+};
+
+
+//test
+console.log(weatherToGiphyMap["13n"]);
