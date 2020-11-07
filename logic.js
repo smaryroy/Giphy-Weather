4fdb76bdbe38c2371e2c9eac526ff70397aa2779
nate <rotarygurter@gmail.com>
2020-11-06 21:26:28 -0600

    initial logic.js file

---
+++ logic.js
@@ -0,0 +1,14 @@
+//openweather api key
+let apiKey = "c0823128cbf8487c457530048730b089";
+//openweather api call url
+let apiURL =
+  "api.openweathermap.org/data/2.5/weather?q=" +
+  cityName +
+  stateCode +
+  "&appid=";
+
+//get city name
+let cityName;
+
+//get statecode
+let stateCode;
