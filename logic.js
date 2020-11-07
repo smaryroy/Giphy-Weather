2bdd06ee6ad378a603af6080f9f62f2b2580d332
nate <rotarygurter@gmail.com>
2020-11-07 10:47:38 -0600

    updated 5day request

--- logic.js
+++ logic.js
@@ -1,12 +1,16 @@
 //openweather api key
 let apiKey = "c0823128cbf8487c457530048730b089";
 //openweather api call url
-let apiURL =
+let currentURL =
   "api.openweathermap.org/data/2.5/weather?q=" +
   cityName +
   "," +
   stateCode +
-  "&appid=" + apiKey;
+  "&appid=" + 
+  apiKey;
+//openweather api for 5 day
+let forecastURL = "api.openweathermap.org/data/2.5/forecast?q="+ cityName "&appid="+ apiKey;
+
 
 //get city name
 let cityName;
@@ -14,11 +18,15 @@ let cityName;
 //get statecode
 let stateCode;
 
-
+//AJAX request for 5day
+$.ajax({
+    url: forecastURL,
+    method: "GET",
+}).then();
 
 //AJAX request
 //
 $.ajax({
-    url: ,
-    method: "GET"
+  url: apiURL,
+  method: "GET",
 }).then();
