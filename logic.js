1ec3abfd606b6333c4c6f6a3937291a1b75b6839
nate <rotarygurter@gmail.com>
2020-11-07 12:10:12 -0600

    got current weather api call working

--- logic.js
+++ logic.js
@@ -1,32 +1,48 @@
 //openweather api key
 let apiKey = "c0823128cbf8487c457530048730b089";
 //openweather api call url
-let currentURL =
-  "api.openweathermap.org/data/2.5/weather?q=" +
-  cityName +
-  "," +
-  stateCode +
-  "&appid=" + 
-  apiKey;
+
 //openweather api for 5 day
-let forecastURL = "api.openweathermap.org/data/2.5/forecast?q="+ cityName "&appid="+ apiKey;
+// let forecastURL =
+//   "api.openweathermap.org/data/2.5/forecast?q=" +
+//   cityName +
+//   "," +
+//   stateCode +
+//   "&appid=" +
+//   apiKey;
+let forecastResponse;
 
-
-//get city name
-let cityName;
-
-//get statecode
-let stateCode;
-
-//AJAX request for 5day
-$.ajax({
-    url: forecastURL,
+//get city name//AJAX request
+//for current weather
+function getForecast(location) {
+  let currentURL =
+    "https://api.openweathermap.org/data/2.5/weather?q=" +
+    location +
+    "&appid=" +
+    apiKey;
+  $.ajax({
+    url: currentURL,
     method: "GET",
-}).then();
+  }).then(function (response) {
+    console.log(response);
+    forecastResponse = response;
+  });
+}
+// let cityName = $("#cityName");
 
-//AJAX request
+// //get statecode
+// let stateCode = $("#stateCode");
+
+// //AJAX request for 5day
+// $.ajax({
+//   url: forecastURL,
+//   method: "GET",
+// }).then();
+getForecast("");
+console.log(forecastResponse);
+//get city, state, weather from response
+
+//store response-city, state, weather in localstorage
+
 //
-$.ajax({
-  url: apiURL,
-  method: "GET",
-}).then();
+//get response[key:value] - send value to giphy call
