09e0335012f69bc1d2c07b789ac6e939ce388713
smary <sziebe@gmail.com>
2020-11-07 12:33:42 -0600

    Correct scope issues in pseduo.js

--- assets/docs/pseudo.js
+++ assets/docs/pseudo.js
@@ -3,28 +3,44 @@
 
 //Giphy Weather PseudoCode
 
-function getForecast(location) {
+// global variables
+let location = "";
+let giphySearchString = "";
+let forecastResponse;
+let returnForecast = {
+	"success": false,
+	"errorMessage": "",
+	"main": "",
+	"description": "",
+	"icon": ""//,
+	//"sevenday":[.....]
+}
 
-	let returnForecast = {
-		"success": false,
-		"errorMessage": "",
-		"main": "",
-		"description": "",
-		"icon": ""//,
-		//"sevenday":[.....]
-	}
 
-	//query openweather API with location value being passed in
+function clearReturnForecast(){
+	//clear for the next query
+	returnForecast.success = false;
+	returnForecast.errorMessage = "";
+	returnForecast.main = "";
+	returnForecast.description = "";
+	returnForecast.icon = "";
+
+}
+
+function getForecast() {
+	//clear the response first
+	clearReturnForecast();
+
+	//query openweather API with location value 
 	//assume the location value has already been validated
 	//if query is successful, then parse the json response 
 		//put values into the the fields of the returnForecast object
 		//ie:  returnForecast.success = true;  returnForecast.main = <insert value from response here>; etc. for all fields
 	
-	return returnForecast;
 }
 
-function validateLocation(location){
-	//the input value provided by the user on the web page is passed in
+function validateLocation(){
+	//the 'location' variable should already be set
 	// this value should be checked to see if it is : 
 		//not undefined
 		//not blank
@@ -39,32 +55,34 @@ function getGiphySearchTerms(weatherIcon){
 	return weatherToGiphyMap[weatherIcon];
 }
 
-function getGiphy(searchString){
-	//pass in the string to search for on the Giphy API
-	//set limit to 1, set offset to a random number between 1 and 100
-	//return embed_url value as string
+function getGiphy(){
+
+	//the 'giphySearchString' variable should be set at the point
+	//use giphySearchString to search for on the Giphy API
+	//set parameter limit to 1, set offset to a random number between 1 and 100
+	//return the value of the embed_url field in the response
 }
 
 
 function giphyWeather(){
 	//call this function when click event is fired
-	let location; //set this value  ...   pull the location input from the input element on the web page
 
 	//if not valid, exit this function
-	if (!validateLocation(location)) {
-		alert("Invalid location input!");  // add a modal instead of an alert
+	if (!validateLocation()) {
+		alert("Invalid location input!");  // todo: add a modal instead of an alert
 		return;
 	}
 
-	//query for forecast
-	let currentForecast = getForecast(location);
-	if (!currentForecast.success) {
+	//query for forecast, location variable should be validated
+	// results go into returnForecast
+	getForecast();
+	if (!returnForecast.success) {
 		alert("Unable to get weather for location " + location + "!");  // add a modal instead of an alert
 		return;
 	}
 
 	//map weather icon to search string
-	let giphySearchString = getGiphySearchTerms(currentForecast.icon);
+	let giphySearchString = getGiphySearchTerms(returnForecast.icon);
 
 	//query for giphy url
 	let giphyUrl = getGiphy(giphySearchString);
@@ -77,6 +95,6 @@ function giphyWeather(){
 	//display each of these items in appropriate elements on main page
 		//  currentForecast.main
 		//  currentForecast.description
-		//  currentForecast.giphyUrl
+		//  giphyUrl
 
 }
